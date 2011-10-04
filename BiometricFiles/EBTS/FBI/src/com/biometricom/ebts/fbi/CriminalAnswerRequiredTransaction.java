ce980f5dff89d2ca780b8ba9509d1c704d3ab360
pb <pb@biometricom.com>
2011-10-04 13:41:54 +0000

    Adding EBTS projects for DOD and FBI

---
+++ BiometricFiles/EBTS/FBI/src/com/biometricom/ebts/fbi/CriminalAnswerRequiredTransaction.java
@@ -0,0 +1,49 @@
+/** 
+ * File:    CriminalAnswerRequiredTransaction.java
+ * Package: com.biometricom.ebts.fbi
+ *
+ * Copyright (c) 2011 BiometriCom, Inc.
+ * http://www.BiometriCom.com
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of BiometriCom, Inc.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with BiometriCom, Inc..
+ *
+ */
+package com.biometricom.ebts.fbi;
+
+import com.biometricom.nist.itl.biometrics.interchange.Transaction;
+import com.biometricom.nist.itl.biometrics.interchange.TransactionDefinition;
+import com.biometricom.nist.itl.biometrics.interchange.TransactionFactory;
+
+
+
+/** 
+ * 
+ *
+ * 
+ * Change History:
+ * Date - Who : Details
+ * --------------------------------------------------------------------
+ * Oct 3, 2011 - Pete Byhre : Created
+ * 
+ * 
+ *
+ */
+public class CriminalAnswerRequiredTransaction extends Transaction
+{
+
+	/* (non-Javadoc)
+	 * @see com.biometricom.nist.itl.biometrics.interchange.Transaction#getMaxRecordsForType(int)
+	 */
+	@Override
+	public int getMaxRecordsForType(int type)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ BiometricFiles/EBTS/FBI/src/com/biometricom/ebts/fbi/Registry.java
@@ -0,0 +1,45 @@
+/** 
+ * File:    Registry.java
+ * Package: com.biometricom.ebts.fbi
+ *
+ * Copyright (c) 2011 BiometriCom, Inc.
+ * http://www.BiometriCom.com
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of BiometriCom, Inc.
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with BiometriCom, Inc..
+ *
+ */
+package com.biometricom.ebts.fbi;
+
+import com.biometricom.nist.itl.biometrics.interchange.Transaction;
+import com.biometricom.nist.itl.biometrics.interchange.TransactionDefinition;
+import com.biometricom.nist.itl.biometrics.interchange.TransactionFactory;
+
+/** 
+ * 
+ *
+ * 
+ * Change History:
+ * Date - Who : Details
+ * --------------------------------------------------------------------
+ * Oct 3, 2011 - Pete Byhre : Created
+ * 
+ * 
+ *
+ */
+public class Registry
+{
+	private Registry()
+	{
+		
+	}
+	
+	public static void registerTransactions() throws Exception
+	{
+		TransactionFactory.registerTransaction(new TransactionDefinition("EBTS-FBI", "9.2", "CAR", Transaction.STD_VERSION_2000, "com.biometricom.ebts.fbi.CriminalAnswerRequiredTransaction"));
+	}
+}
